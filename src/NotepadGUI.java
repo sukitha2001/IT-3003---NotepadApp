caa551f96a611966da37df74b6cd32bf75a09a5d
Sukitha <sukitharathnayake001@gmail.com>
2025-09-06 22:43:50 +0530

    Adding the toolbar with file menu

--- src/NotepadGUI.java
+++ src/NotepadGUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class NotepadGUI extends JFrame {
     public  NotepadGUI() {
@@ -6,5 +7,28 @@ public class NotepadGUI extends JFrame {
         setSize(400,500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        addGuiComponents();
+    }
+    public void addGuiComponents(){
+        addToolbar();
+    }
+    public void addToolbar(){
+
+
+        JToolBar toolBar = new JToolBar();
+
+        //Menu bar
+        JMenuBar menuBar = new JMenuBar();
+        toolBar.add(menuBar);
+
+        //Add Menus
+        menuBar.add(addFileMenu());
+
+        add(toolBar, BorderLayout.NORTH);
+    }
+    private  JMenu addFileMenu(){
+        JMenu fileMenu = new JMenu("File");
+        return  fileMenu;
     }
 }
