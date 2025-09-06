a590c65f92543507b94404d91b2a90c6ed7fc779
Sukitha <sukitharathnayake001@gmail.com>
2025-09-06 22:28:01 +0530

    init commit

---
+++ src/NotepadGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class NotepadGUI extends JFrame {
+    public  NotepadGUI() {
+        super("Notepad");
+        setSize(400,500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
