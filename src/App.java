314c0c68738221690f529fcf182434d6c4875cef
Sukitha <sukitharathnayake001@gmail.com>
2025-09-06 22:31:40 +0530

    generating notepad frame

--- src/App.java
+++ src/App.java
@@ -1,2 +1,12 @@
+import javax.swing.*;
+
 public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new NotepadGUI().setVisible(true);
+            }
+        });
+    }
 }
